500a35d201c8aaf07c0293b7ed52d30817817296
Markos-MG <147771076+Markos-MG@users.noreply.github.com>
2024-03-28 13:24:12 +0100

    Prueba Marcos
    kijote

---
+++ Assets/Script/Prueba Marcos.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// aprueba guarraaaa q tan quedao 7
+public class PruebaMarcos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

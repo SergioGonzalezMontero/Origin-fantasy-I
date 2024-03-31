f986136981331710e6ceba083a7a7a8b9c6d4ee7
Markos-MG <147771076+Markos-MG@users.noreply.github.com>
2024-03-31 14:19:25 +0200

    Movimiento

---
+++ Assets/Scripts/Per_Movimiento.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Per_Movimiento : MonoBehaviour
+{
+
+    [SerializeField] private float velocidad;
+
+    private Rigidbody2D rig;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movimiento();
+    }
+
+    private void Movimiento()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical= Input.GetAxis("Vertical");
+
+        rig.velocity = new Vector2(horizontal, vertical) * velocidad;
+        anim.SetFloat("Velocidad", Mathf.Abs(rig.velocity.magnitude));
+
+        if(horizontal >  0)
+        {
+            anim.SetInteger("Direccion", 1);
+
+        }
+        else if (horizontal < 0)
+        {
+            anim.SetInteger("Direccion", 3);
+        }
+
+        if(vertical > 0)
+        {
+            anim.SetInteger("Direccion", 0);
+
+        }
+        else if (vertical < 0)
+        {
+            anim.SetInteger("Direccion", 2);
+        }
+    }
+}

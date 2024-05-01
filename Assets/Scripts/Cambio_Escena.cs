8b2241369f8a937ab089604dac1b13f03cb71b6f
Markos-MG <147771076+Markos-MG@users.noreply.github.com>
2024-05-01 15:02:25 +0200

    Cambio de escenas 0.7

---
+++ Assets/Scripts/Cambio_Escena.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cambio_Escena : MonoBehaviour
+{
+    public int num_Escena;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            PlayerPrefs.SetString("EscenaAnterior", SceneManager.GetActiveScene().name);
+            PlayerPrefs.Save();
+
+            switch (SceneManager.GetActiveScene().name.ToString())
+            {
+                case "Calle":
+                    SceneManager.LoadScene("Entrada");
+
+                    break;
+                case "Entrada":
+                    if(num_Escena == 1)
+                    {
+                        SceneManager.LoadScene("Calle");
+                    } else if(num_Escena == 2) 
+                    {
+                        SceneManager.LoadScene("Patio_Inferior");
+                    }
+                    
+                    break;
+                case "Patio_Inferior":
+                    if (num_Escena == 1)
+                    {
+                        SceneManager.LoadScene("Entrada");
+                    }
+                    else if (num_Escena == 2)
+                    {
+                        SceneManager.LoadScene("Patio_Superior");
+                    }
+
+                    break;
+                case "Patio_Superior":
+                    if (num_Escena == 1)
+                    {
+                        SceneManager.LoadScene("Patio_Inferior");
+                    }
+                    else if (num_Escena == 2)
+                    {
+                        SceneManager.LoadScene("EdificioC_P0");
+                    }
+
+                    break;
+
+                case "EdificioC_P0":
+                    if (num_Escena == 4)
+                    {
+                        SceneManager.LoadScene("Patio_Superior");
+                    }
+                    else if (num_Escena == 5)
+                    {
+                        SceneManager.LoadScene("EdificioC_C1");
+                    }else if (num_Escena == 6)
+                    {
+                        SceneManager.LoadScene("EdificioC_P1");
+                    }
+
+                    break;
+            }
+            
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Posicion_Inicial.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Posicion_Inicial : MonoBehaviour
+{
+    public Transform Inicio1;
+    public Transform Inicio2;
+    public Transform Inicio3;
+
+    private void Start()
+    {
+        string escenaAnterior = PlayerPrefs.GetString("EscenaAnterior");
+
+        // Determinar la posición inicial del jugador según la escena anterior
+        Debug.Log(escenaAnterior);
+        switch (escenaAnterior)
+        {
+            case "Calle":
+                transform.position = Inicio1.position;
+                break;
+            case "Entrada":
+                if (SceneManager.GetActiveScene().name.ToString().Equals("Calle"))
+                {
+                    transform.position = Inicio2.position;
+                }
+                else if (SceneManager.GetActiveScene().name.ToString().Equals("Patio_Inferior"))
+                {
+                    transform.position = Inicio1.position;
+                }
+                break;
+            case "Patio_Inferior":
+                if (SceneManager.GetActiveScene().name.ToString().Equals("Entrada"))
+                {
+                    transform.position = Inicio2.position;
+                }
+                else if (SceneManager.GetActiveScene().name.ToString().Equals("Patio_Superior"))
+                {
+                    transform.position = Inicio1.position;
+                }
+                break;
+            case "Patio_Superior":
+                if (SceneManager.GetActiveScene().name.ToString().Equals("Patio_Inferior"))
+                {
+                    transform.position = Inicio2.position;
+                }
+                else if (SceneManager.GetActiveScene().name.ToString().Equals("EdificioC_P0"))
+                {
+                    transform.position = Inicio1.position;
+                }
+                break;
+            case "EdificioC_P0":
+                if (SceneManager.GetActiveScene().name.ToString().Equals("Patio_Superior"))
+                {
+                    transform.position = Inicio2.position;
+                }else if (SceneManager.GetActiveScene().name.ToString().Equals("EdificioC_C0"))
+                {
+                    transform.position = Inicio1.position;
+                }else if (SceneManager.GetActiveScene().name.ToString().Equals("EdificioC_P1"))
+                {
+                    transform.position = Inicio1.position;
+                }
+                break;
+            case "EdificioC_C0":
+                transform.position = Inicio2.position;
+
+                break;
+            case "EdificioC_P1":
+                if (SceneManager.GetActiveScene().name.ToString().Equals("EdificioC_P0"))
+                {
+                    transform.position = Inicio3.position;
+                }
+                else if (SceneManager.GetActiveScene().name.ToString().Equals("EdificioC_C1"))
+                {
+                    transform.position = Inicio1.position;
+                }
+                break;
+        }
+        // Añadir más condiciones según sea necesario
+    }
+}

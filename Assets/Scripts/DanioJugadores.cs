ce1cf4704c3c05e2dba0df43fd32eb9f11b67ea7
AdrianMorneo <116754888+AdrianMorneo@users.noreply.github.com>
2024-05-26 18:03:18 +0200

    Intento de dialogo en con profe Sergio y pequeños ajustes en Patio Superior

--- Assets/Scripts/DanioJugadores.cs
+++ Assets/Scripts/DanioJugadores.cs
@@ -1,34 +1,61 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DanioJugadores : MonoBehaviour
 {
+
     int veces = 0;
     public float tiempoDano;
-    private bool nuevaQuemadura = true; // Iniciar como true para permitir el primer daño
+    private bool nuevaQuemadura = false;
     private GameObject playerRef;
-
+    // Start is called before the first frame update
     void Start()
     {
-        tiempoDano = 5f;
+        tiempoDano = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("NPC") && nuevaQuemadura)
+        if (other.tag == "NPC")
         {
             veces++;
             Debug.Log("Quemado " + veces);
             playerRef = other.gameObject;
-            other.GetComponent<Per_Movimiento>().DanoJugador(1);
-            nuevaQuemadura = false; // Desactivar la posibilidad de daño por un tiempo
-            StartCoroutine(ReactivarQuemadura());
+            other.gameObject.GetComponent<Per_Movimiento>().DanoJugador(1);
+            StartCoroutine(newQuemaduraCorrutina());
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "NPC")
+        {
+            if (nuevaQuemadura)
+            {
+                nuevaQuemadura = false;
+                playerRef = other.gameObject;
+                StartCoroutine(newQuemaduraCorrutina());
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "NPC")
+        {
+            StopAllCoroutines();
         }
     }
 
-    IEnumerator ReactivarQuemadura()
+    IEnumerator newQuemaduraCorrutina()
     {
         yield return new WaitForSeconds(tiempoDano);
-        nuevaQuemadura = true; // Reactivar la posibilidad de daño
+        nuevaQuemadura = true;
+        playerRef.GetComponent<Per_Movimiento>().DanoJugador(1);
     }
 }

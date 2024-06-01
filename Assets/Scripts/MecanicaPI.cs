492dc41ab03057c032aca97909f23603fe67992c
AdrianMorneo <116754888+AdrianMorneo@users.noreply.github.com>
2024-06-01 20:26:00 +0200

    Minijuego psasillo inferior terminado

--- Assets/Scripts/MecanicaPI.cs
+++ Assets/Scripts/MecanicaPI.cs
@@ -8,9 +8,12 @@ public class MecanicaPI : MonoBehaviour
     private Vector2 moveDirection = Vector2.zero; // Dirección actual de movimiento
     private bool canMove = true; // Indica si el personaje puede moverse o no
     private Vector2 lastMoveDirection; // Última dirección de movimiento
+    private Animator anim; // Componente Animator para controlar las animaciones
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();// Obtener el componente Animator
         moveDirection = -lastMoveDirection;
         rb.velocity = moveDirection * speed;
     }
@@ -20,11 +23,14 @@ public class MecanicaPI : MonoBehaviour
         if (rb.velocity == Vector2.zero)
         {
             canMove = true;
+            //anim.SetBool("Parado",false); // Detener la animación de movimiento
         }
 
         // Si el personaje puede moverse y se presiona una tecla de dirección
         if (canMove && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) ||
-                        Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
+                        Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) ||
+                        Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+                        Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
         {
             // Obtener la dirección de movimiento basada en la tecla presionada
             float horizontalInput = Input.GetAxis("Horizontal");
@@ -46,6 +52,27 @@ public class MecanicaPI : MonoBehaviour
 
             // No permitir que se cambie la dirección del personaje mientras este se está moviendo
             canMove = false;
+
+            //Cambio la animacion del personaje segun la direccion seleccionada
+            if (horizontalInput > 0)
+            {
+                anim.SetInteger("Direccion", 1);
+
+            }
+            else if (horizontalInput < 0)
+            {
+                anim.SetInteger("Direccion", 3);
+            }
+
+            if (verticalInput > 0)
+            {
+                anim.SetInteger("Direccion", 0);
+
+            }
+            else if (verticalInput < 0)
+            {
+                anim.SetInteger("Direccion", 2);
+            }
         }
     }
 
@@ -73,9 +100,5 @@ public class MecanicaPI : MonoBehaviour
     void OnEnable()
     {
         rb = GetComponent<Rigidbody2D>();
-       
     }
 }
-
-
-
--- Assets/Scripts/ZonaActivacionMPI.cs
+++ Assets/Scripts/ZonaActivacionMPI.cs
@@ -6,7 +6,6 @@ public class ZonaActivacionMPI : MonoBehaviour
 {
     public Per_Movimiento movementScript1;
     public MecanicaPI movementScript2;
-    private static int entraOsale = 1;
 
     private void Start()
     {
@@ -20,70 +19,22 @@ public class ZonaActivacionMPI : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //StartCoroutine(DisableColliderTemporarily());
-
-            entraOsale++;
-            if (entraOsale % 2 == 0)
-            {
+            
                 Debug.Log("Entra");
                 movementScript1.enabled = false;
                 movementScript2.enabled = true;
-            }
-            else
-            {
-                Debug.Log("Sale");
-                movementScript1.enabled = true;
-                movementScript2.enabled = false;
-            }
-            Debug.Log("El número es: " + entraOsale);
-
-            // Reposiciona al jugador al lado opuesto del collider
-            //RepositionPlayer(other);
-
+        
         }
     }
-
-    /*void RepositionPlayer(Collider2D playerCollider)
+    void OnTriggerExit2D(Collider2D other)
     {
-        // Obtenemos los límites del collider de la zona
-        Bounds bounds = GetComponent<Collider2D>().bounds;
-        Vector3 playerPosition = playerCollider.transform.position;
-
-        // Reposiciona al jugador en el eje Y
-        if (playerPosition.y < bounds.center.y)
+        if (other.CompareTag("Player"))
         {
-            // Si el jugador viene desde abajo, reposiciona arriba
-            playerPosition.y = bounds.max.y + 1; // Ajusta '1' al espacio deseado entre el jugador y el collider
+            Debug.Log("Sale");
+            movementScript1.enabled = true;
+            movementScript2.enabled = false;
         }
-        else
-        {
-            // Si el jugador viene desde arriba, reposiciona abajo
-            playerPosition.y = bounds.min.y - 1; // Ajusta '1' al espacio deseado entre el jugador y el collider
-        }
-
-        // Asigna la nueva posición al transform del jugador
-        playerCollider.transform.position = playerPosition;
     }
-
-    IEnumerator DisableColliderTemporarily()
-    {
-        Collider2D collider = GetComponent<Collider2D>();
-        if (collider != null)
-        {
-            // Desactiva el collider
-            collider.enabled = false;
-
-            // Espera medio segundo
-            yield return new WaitForSeconds(1f);
-
-            // Reactiva el collider
-            collider.enabled = true;
-        }
-        else
-        {
-            Debug.LogError("No se encontró el Collider2D en el GameObject.");
-        }
-    }*/
 }
 
 

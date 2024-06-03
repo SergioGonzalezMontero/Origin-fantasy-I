9d870fe58d05386f043e044aaf7736d791924ea9
Sergio Gonzalez <gonmonsergio@gmail.com>
2024-06-03 22:00:24 +0200

    arreglado convers y menu pausa

--- Assets/Scripts/Dialogos/S_DialogueSpeaker.cs
+++ Assets/Scripts/Dialogos/S_DialogueSpeaker.cs
@@ -148,6 +148,7 @@ public class S_DialogueSpeaker : MonoBehaviour
         }
         else
         {
+            conversacionesDisponibles[indexDeConversaciones].finalizado = false;
             return true;
         }
     }
